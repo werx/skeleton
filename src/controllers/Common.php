ce1e2c3c5c4b58e43183e312691d2c9260fca203
Jonathan Barket <jonathan.barket@ark.org>
2015-01-23 15:16:54 -0600

    Added Common controller to house universal bootstrapping for controllers. Moved non-specific code from Home to Common.

---
+++ src/controllers/Common.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace werx\Skeleton\Controllers;
+
+use Illuminate\Database\Capsule\Manager as Model;
+use werx\Core\Controller;
+use werx\Core\Database as DB;
+use werx\Messages\Messages;
+
+class Common extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        // Set our default template.
+        $this->template->layout('layouts/default');
+
+        // Set some variables for all views.
+        $this->template->page_title = 'Werx Skeleton';
+
+        // Load our primary config file.
+        $this->config->load('config');
+
+        // Load the messages library, passing in an instance of the session.
+        Messages::getInstance($this->session);
+
+        // Set the default decorator to twitter bootstrap.
+        Messages::setDecorator(new \werx\Messages\Decorators\Bootstrap);
+
+        // And our database config file.
+        #$this->config->load('database');
+
+        // Initialize our database.
+        #DB::init($this->config->get('dsn'));
+    }
+
+}
